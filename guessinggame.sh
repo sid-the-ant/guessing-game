c7e83918577bb842550af86d9cf1a821bb28fd2b
sid-the-ant <siddhantbhat2010@gmail.com>
2020-05-08 00:19:36 +0530

    Updated makefile to lines and time/date

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,20 +3,16 @@ function check-answer
 	if [[ $guess -gt $a ]]
 	then
 		echo 'too high!'
-
 	elif [[ $guess -lt $a ]]
 	then
 		echo 'too low!'
-
 	else
 		echo 'Congratulations! You made the correct guess!'
 	fi
 }
 
 a=$( ls | wc -l )
-
 guess=-1
-
 while [[ ! $a -eq $guess ]]
 do
 	read -p "Enter your guess $num: " guess
